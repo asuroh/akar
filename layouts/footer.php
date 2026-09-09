bc399d871a61029620ec29ee39073a7222517ed9
Thoriq Aziz Asuro <thoriq.aziz@sicepat.com>
2026-09-09 07:00:39 +0700

    feat: add new page login

---
+++ layouts/footer.php
@@ -0,0 +1,14 @@
+<!--begin::Footer-->
+      <footer class="app-footer">
+        <div class="float-end d-none d-sm-inline">Anything you want</div>
+        <strong>
+          Copyright &copy; 2014-2026&nbsp;
+          <a href="https://adminlte.io" class="text-decoration-none">SMK Kawung 1 Surabaya</a>.
+        </strong>
+        All rights reserved.
+      </footer>
+      <!--end::Footer-->
+
+    </div>
+  </body>
+</html>
--- views/dashboard.php
+++ views/dashboard.php
@@ -1,7 +1,12 @@
 <?php
     session_start();
-
-    if (isset($_SESSION['status'])|| $_SESSION['status'] != "login"){
-        
+    if (!isset($_SESSION['status']) || $_SESSION['status'] != "login"){
+        header("location: login.php?pesan=belum_login");
+        exit;
     }
+
+    include '../layouts/header.php';
+    include '../layouts/sidebar.php';
+
+    include '../layouts/footer.php';
 ?>
